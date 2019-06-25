ab3504992d4358e078d679e14cf7a810a55694ff
Gertjan Rösken <gertjan@goeiePeer.nl>
2019-06-25 10:03:09 +0200

    Initial Commit

---
+++ MyTodoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyTodoey
+//
+//  Created by Gertjan Rösken on 25/06/2019.
+//  Copyright © 2019 Gertjan Rösken. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
